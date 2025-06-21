fe30c144da4e780723e165dc072b37f34137c7d0
FydorZbinyakov <fydorzbinyakov@gmail.com>
2025-06-21 23:58:20 +0300

    fix deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello world!!!123';
+    return 'Hello world!!!123!!!';
   }
 }
