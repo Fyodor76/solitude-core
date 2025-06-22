41446898286b72e71534acba9d2b9a5f8103a2e7
FydorZbinyakov <fydorzbinyakov@gmail.com>
2025-06-22 23:59:20 +0300

    fix db config

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -14,5 +14,5 @@ export const createDatabaseConfig = (
   database: configService.get<string>('POSTGRES_DB'),
   models: [User, Test],
   autoLoadModels: true,
-  synchronize: false,
+  synchronize: true,
 });
