33c3d0fea49552a252c71cdbe2999ea723ecde76
FydorZbinyakov <fydorzbinyakov@gmail.com>
2025-06-30 02:40:41 +0300

    test request

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello world!!!1234!!!1321232';
+    return 'Test request';
   }
 }
