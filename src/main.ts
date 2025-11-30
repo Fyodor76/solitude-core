c564593e685c7155e75153566600f43961751777
FydorZbinyakov <fydorzbinyakov@gmail.com>
2025-11-30 21:58:51 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: ['https://solitude-store.ru', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
