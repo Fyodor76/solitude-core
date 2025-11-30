bc3d9319299f7eac0a5f27b64efd202b75ecc68f
FydorZbinyakov <fydorzbinyakov@gmail.com>
2025-11-30 22:20:50 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,6 @@ import { UniversalExceptionsFilter } from './common/filters/universal-all-except
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['https://solitude-store.ru', 'http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
